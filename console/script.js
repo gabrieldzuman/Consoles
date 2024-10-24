8ac29d38929cf1b69776f4743611a971888cc1bd
Gabriel Dzuman <83187468+gabrieldzuman@users.noreply.github.com>
2024-10-24 12:08:52 -0300

    Update script.js

--- console/script.js
+++ console/script.js
@@ -2,8 +2,80 @@ const left = document.querySelector(".left");
 const right = document.querySelector(".right");
 const container = document.querySelector(".container");
 
-left.addEventListener("mouseenter", () => container.classList.add("hover-left"));
-left.addEventListener("mouseleave", () => container.classList.remove("hover-left"));
+function debounce(func, delay = 100) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => func.apply(this, args), delay);
+  };
+}
 
-right.addEventListener("mouseenter", () => container.classList.add("hover-right"));
-right.addEventListener("mouseleave", () => container.classList.remove("hover-right"));
+function addClassWithCallback(element, className, callback) {
+  if (element) {
+    element.classList.add(className);
+    callback && callback(className);
+  }
+}
+
+function removeClassWithCallback(element, className, callback) {
+  if (element) {
+    element.classList.remove(className);
+    callback && callback(className);
+  }
+}
+
+function onMouseEnterCallback(className) {
+  console.log(`Classe ${className} foi adicionada ao container`);
+}
+
+function onMouseLeaveCallback(className) {
+  console.log(`Classe ${className} foi removida do container`);
+}
+
+if (left && right && container) {
+  left.addEventListener(
+    "mouseenter",
+    debounce(() => addClassWithCallback(container, "hover-left", onMouseEnterCallback), 200)
+  );
+  left.addEventListener(
+    "mouseleave",
+    debounce(() => removeClassWithCallback(container, "hover-left", onMouseLeaveCallback), 200)
+  );
+
+  right.addEventListener(
+    "mouseenter",
+    debounce(() => addClassWithCallback(container, "hover-right", onMouseEnterCallback), 200)
+  );
+  right.addEventListener(
+    "mouseleave",
+    debounce(() => removeClassWithCallback(container, "hover-right", onMouseLeaveCallback), 200)
+  );
+}
+
+function handleTouchStart(event, className) {
+  addClassWithCallback(container, className, onMouseEnterCallback);
+}
+
+function handleTouchEnd(event, className) {
+  removeClassWithCallback(container, className, onMouseLeaveCallback);
+}
+
+left.addEventListener("touchstart", (e) => handleTouchStart(e, "hover-left"));
+left.addEventListener("touchend", (e) => handleTouchEnd(e, "hover-left"));
+
+right.addEventListener("touchstart", (e) => handleTouchStart(e, "hover-right"));
+right.addEventListener("touchend", (e) => handleTouchEnd(e, "hover-right"));
+
+const style = document.createElement("style");
+style.textContent = `
+  .container {
+    transition: transform 0.3s ease-in-out;
+  }
+  .hover-left .container {
+    transform: translateX(-50px);
+  }
+  .hover-right .container {
+    transform: translateX(50px);
+  }
+`;
+document.head.append(style);
